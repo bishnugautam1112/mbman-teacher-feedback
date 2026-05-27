35cbb279876d81cd8300fd16401ca7666bf4ca7a
bishnugautam1112 <bishnu.gautam2005@gmail.com>
2026-05-28 02:46:08 +0545

    Add Google Site Verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 export const metadata: Metadata = {
   title: "Anomalous Teacher Feedback System | MBMAN",
   description: "A secure, 100% anonymous feedback and review system for teachers at Madan Bhandari Memorial Academy Nepal.",
+  verification: {
+    google: "ro693PVgKzq1RCtDvyrw56_f6T0adACF8VI6kIjicBw",
+  },
 };
 
 export default function RootLayout({
