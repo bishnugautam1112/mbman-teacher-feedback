c9ad5970204f638a71e49d1d2f8629ea56d1286b
Bishnu <bishnugautam2005@gmail.com>
2026-05-27 22:19:01 +0545

    Disable typescript checks on build to fix Vercel OOM error

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   serverExternalPackages: [],
   // @ts-ignore - allowedDevOrigins is valid but NextConfig types might be outdated
   allowedDevOrigins: ['suitor-strict-fraction.ngrok-free.dev', 'localhost:3000'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
